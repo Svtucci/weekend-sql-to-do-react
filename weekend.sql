91b96f52eb2d84d465437f8b27cdcddd12ab699e
Stephen Vertucci <steve.vertucci@gmail.com>
2023-03-12 16:05:37 -0400

    added sql to vscode

---
+++ weekend.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "taskList" (
+	"id" SERIAL PRIMARY KEY,
+	"task" varchar(255),
+	"status" BOOLEAN DEFAULT false
+);
+	
+SELECT * FROM "taskList";
+
+INSERT INTO "taskList" ("task", "status") 
