4c9353428042ae9877f58e3668a9927755fab0c6
Stephen Vertucci <steve.vertucci@gmail.com>
2023-03-12 16:03:18 -0400

    created postico data, began constructions of CRUD

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js');
 
-// GET
+// GET will pull from the weekend-to-do-app DB
+router.get('/', (req, res) => {
+    let queryDataBase = 'SELECT * FROM "taskList";';
+    pool.query(queryDataBase).then((result) => {
+        res.send(result.row);
+    }).catch((error) => {
+        console.log(`Error in GET ${error}`)
+        res.sendStatus(500);
+    })
+})
 
-// POST
+// POST will send info to DB 
 
-// PUT
+// PUT will take updated info from DOM, send to DB to be udpated and then resent to DOM
 
-// DELETE
+// DELETE will delete w/e id we choose 
 
 module.exports = router;
