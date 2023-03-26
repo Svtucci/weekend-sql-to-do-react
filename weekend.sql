b1c0bd35d151f6ef56cc40e6be72e3690b1dd772
Stephen Vertucci <steve.vertucci@gmail.com>
2023-03-25 21:58:55 -0400

    created new DB with compelted status with boolean

--- weekend.sql
+++ weekend.sql
@@ -1,10 +1,13 @@
 CREATE TABLE "taskList" (
 	"id" SERIAL PRIMARY KEY,
 	"task" varchar(255),
-	"description" varchar(255)
+	"description" varchar(255),
+	"completionstatus" BOOLEAN DEFAULT FALSE
 );
 	
 SELECT * FROM "taskList";
 
-INSERT INTO "taskList" ("task", "description") 
-			VALUES ('Mow the lawn', 'The grass grew, go mow it');
+INSERT INTO "taskList" ("task", "description", "completionstatus") 
+			VALUES ('Mow the lawn', 'The grass grew, go mow it', FALSE);
+
+			
